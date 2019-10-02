1e60e4bb63b50b603ed32f7d3acba227af0b7233
nasum <tomato.wonder.life@gmail.com>
2019-10-03 02:31:55 +0900

    add vue

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,48 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   mode: process.env.NODE_ENV || "development",
   entry: {
-    background: "./src/background.ts"
+    background: "./src/background.ts",
+    popup: "./src/popup.ts"
   },
   output: {
     filename: '[name].js',
     path: __dirname + '/dist'
   },
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: [".ts", ".vue"],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.resolve(__dirname, 'src'),
+    }
   },
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: [{
+          loader: "ts-loader",
+          options: { appendTsSuffixTo: [/\.vue$/] }
+        }],
         exclude: /node_modules/
       }
     ]
@@ -27,12 +52,14 @@ module.exports = {
       {
         from: "src/manifest.json",
         to: "manifest.json"
-      },
-      {
-        from: "src/popup.html",
-        to: "popup.html"
       }
     ]),
+    new HtmlWebpackPlugin({
+      filename: 'popup.html',
+      chunks: ['popup'],
+      template: 'src/popup.html'
+    }),
     new CleanWebpackPlugin(),
+    new VueLoaderPlugin()
   ]
 }
