61fc5af871fd6a38f80de2ea0e3655c64700d0a8
nasum <tomato.wonder.life@gmail.com>
2019-10-03 01:28:10 +0900

    start by techbookfest page

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,14 @@
-chrome.tabs.onUpdated.addListener((tabId) => {
-  chrome.pageAction.show(tabId);
-});
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'techbookfest.org' },
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ])
+  })
+})
